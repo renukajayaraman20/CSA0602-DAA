69021e5213a4e2583664c7bb77762583570b4e1b
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-07-27 11:29:22 +0530

    Delete Topic 1 directory

--- Topic 1/exp7_output.py
+++
@@ -1,6 +0,0 @@
-Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-==== RESTART: C:/Users/Renuka Devi/OneDrive/Documents/DAA/practical/exp7.py ====
-Enter array elements: 45 89 94 12 1 3
-Sorted Array: [1, 3, 12, 45, 89, 94]
