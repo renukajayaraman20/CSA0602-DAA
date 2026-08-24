b1d32e6cff9a8d183e12e5a7ebcad31523f9504e
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:50:12 +0530

    Update and rename exp3-3.4.py to Playlist Reordering by Recently Added.py

--- Topic 2/Playlist Reordering by Recently Added.py
+++ Topic 2/Playlist Reordering by Recently Added.py
@@ -21,3 +21,18 @@ while i > 0 and playlist[i - 1][1] > playlist[i][1]:
 print("Updated Playlist:")
 for song in playlist:
     print(song)
+#-----------OUTPUT-----------#
+Enter number of songs: 3
+Enter song name 1: INTRO
+Enter duration: 120
+Enter song name 2: CHILL BEAT
+Enter duration: 210
+Enter song name 3: MELODY
+Enter duration: 150
+Enter new song name: LONG JAM
+Enter new song duration: 340
+Updated Playlist:
+('INTRO', 120)
+('CHILL BEAT', 210)
+('MELODY', 150)
+('LONG JAM', 340)
