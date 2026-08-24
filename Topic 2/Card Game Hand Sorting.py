9d07c420cf04a92cbbb207a968dd7fc38cf24df8
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:44:30 +0530

    Include output example for card game hand sorting
    Added output example for card sorting program.

--- Topic 2/Card Game Hand Sorting.py
+++ Topic 2/Card Game Hand Sorting.py
@@ -1,6 +1,7 @@
 n = int(input("Enter number of cards: "))
 
 hand = []
+
 for i in range(n):
     hand.append(int(input(f"Enter card {i+1}: ")))
 
@@ -12,13 +13,30 @@ size = len(hand)
 
 for i in range(size - 1):
     swapped = False
+
     for j in range(size - 1 - i):
         if hand[j] > hand[j + 1]:
             hand[j], hand[j + 1] = hand[j + 1], hand[j]
             swapped = True
+
     passes += 1
+
     if not swapped:
         break
 
 print("Sorted Hand:", hand)
 print("Number of Passes:", passes)
+print("Time Complexity: O(n^2)")
+print("Best Case: O(n)")
+#------------OUTPUT-----------#
+Enter number of cards: 7
+Enter card 1: 2
+Enter card 2: 4
+Enter card 3: 6
+Enter card 4: 8
+Enter card 5: 9
+Enter card 6: 11
+Enter card 7: 13
+Enter new card: 7
+Sorted Hand: [2, 4, 6, 7, 8, 9, 11, 13]
+Number of Passes: 5
