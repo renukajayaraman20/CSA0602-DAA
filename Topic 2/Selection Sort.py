2c839098d512b5b025f66451a329d63840f3d38a
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:30:03 +0530

    Update and rename exp1-1.1.py to Selection Sort.py

--- Topic 2/Selection Sort.py
+++ Topic 2/Selection Sort.py
@@ -17,3 +17,14 @@ for i in range(k):
     arr[i], arr[max_idx] = arr[max_idx], arr[i]
 
 print("Top", k, "scores:", arr[:k])
+#----------OUTPUT--------#
+Enter score 3: 95
+Enter score 4: 74
+Enter score 5: 85
+Enter score 6: 65
+Enter score 7: 12
+Enter score 8: 88
+Enter score 9: 94
+Enter score 10: 78
+Enter how many top scores you want: 5
+Top 5 scores: [95, 94, 88, 85, 78]
