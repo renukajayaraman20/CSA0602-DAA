c2687f7635c59e8b7bf1d33768ebc5a27037714e
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:22:49 +0530

    Update and rename exp5.py to Matrix Multiplication.py

--- Topic 1/Matrix Multiplication.py
+++ Topic 1/Matrix Multiplication.py
@@ -2,6 +2,7 @@ r1 = int(input("Enter no.of rows of Matrix A: "))
 c1 = int(input("Enter no.of columns of Matrix A: "))
 
 A = []
+
 print("Enter Matrix A:")
 for i in range(r1):
     row = list(map(int, input().split()))
@@ -14,6 +15,7 @@ if c1 != r2:
     print("Matrix multiplication not possible.")
 else:
     B = []
+
     print("Enter Matrix B:")
     for i in range(r2):
         row = list(map(int, input().split()))
@@ -29,3 +31,20 @@ else:
     print("Result Matrix:")
     for row in result:
         print(row)
+
+    print("Time Complexity: O(n^3)")
+        #----------------OUTPUT-------------#
+Enter no.of rows of Matrix A: 2
+Enter no.of columns of Matrix A: 2
+Enter Matrix A:
+1 2
+3 4
+Enter rows of Matrix B: 2
+Enter columns of Matrix B: 2
+Enter Matrix B:
+5 6 
+7 8
+Result Matrix:
+[19, 22]
+[43, 50]
+Time Complexity: O(n^3)
