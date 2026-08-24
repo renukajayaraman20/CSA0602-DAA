6f157e372799784e60051cf0eaebc1603a4ce346
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:26:10 +0530

    Update and rename exp8.py to Tower of Hanoi.py

--- Topic 1/Tower of Hanoi.py
+++ Topic 1/Tower of Hanoi.py
@@ -10,3 +10,12 @@ def tower_of_hanoi(n, source, auxiliary, destination):
 n = int(input("Enter number of disks: "))
 
 tower_of_hanoi(n, 'A', 'B', 'C')
+#------------OUTPUT---------#
+Enter number of disks: 3
+Move Disk 1 from A to C
+Move Disk 2 from A to B
+Move Disk 1 from C to B
+Move Disk 3 from A to C
+Move Disk 1 from B to A
+Move Disk 2 from B to C
+Move Disk 1 from A to C
