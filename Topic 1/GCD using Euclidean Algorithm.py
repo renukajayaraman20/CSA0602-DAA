a8cbca71929c6816490bef6c152b29f8c1cb5725
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:27:07 +0530

    Update and rename exp9.py to GCD using Euclidean Algorithm.py

--- Topic 1/GCD using Euclidean Algorithm.py
+++ Topic 1/GCD using Euclidean Algorithm.py
@@ -7,3 +7,7 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
 print("GCD =", gcd(a, b))
+3-------------OUTPUT--------#
+Enter first number: 48
+Enter second number: 18
+GCD = 6
