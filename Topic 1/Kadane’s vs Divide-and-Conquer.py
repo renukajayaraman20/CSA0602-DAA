418d91447fa9378e3bd0b8b8c908c7bdf01e97df
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:13:06 +0530

    Fix indentation and formatting in Kadane's algorithm

--- Topic 1/Kadane’s vs Divide-and-Conquer.py
+++ Topic 1/Kadane’s vs Divide-and-Conquer.py
@@ -8,4 +8,6 @@ for i in range(1, len(arr)):
     max_global = max(max_global, max_current)
 
 print("Maximum Subarray Sum:", max_global)
-5
+#---------OUTPUT----------#
+Enter array elements: -2 -3 4 -1 -2 1 5 -3
+Maximum Subarray Sum: 7
