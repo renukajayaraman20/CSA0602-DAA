07f09a9f9d9c417c398252744197f1108abde651
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:33:03 +0530

    Update and rename exp1-1.3.py to Library Book Reordering.py

--- Topic 2/Library Book Reordering.py
+++ Topic 2/Library Book Reordering.py
@@ -1,5 +1,6 @@
 n = int(input("Enter number of books: "))
 books = []
+
 for i in range(n):
     val = int(input(f"Enter Book ID {i+1}: "))
     books.append(val)
@@ -9,9 +10,11 @@ moves = 0
 
 for i in range(n - 1):
     min_idx = i
+
     for j in range(i + 1, n):
         if a[j] < a[min_idx]:
             min_idx = j
+
     if min_idx != i:
         a[i], a[min_idx] = a[min_idx], a[i]
         moves += 1
@@ -19,3 +22,15 @@ for i in range(n - 1):
 print("Reordered shelf:", a)
 print("Total physical moves:", moves)
 print("Maximum possible moves (n-1):", n - 1)
+print("Time Complexity: O(n^2)")
+#------------OUTPUT-------------#
+Enter Book ID 2: 102
+Enter Book ID 3: 250
+Enter Book ID 4: 1119
+Enter Book ID 5: 199
+Enter Book ID 6: 400
+Enter Book ID 7: 150
+Reordered shelf: [102, 150, 199, 250, 305, 400, 1119]
+Total physical moves: 5
+Maximum possible moves (n-1): 6
+Time Complexity: O(n^2)
