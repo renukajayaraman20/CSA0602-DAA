32925827e54de8448ef51b4e10cf838b94f36589
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:41:39 +0530

    Update and rename exp2-2.3.py to Bubble Sort Visualization Tool.py

--- Topic 2/Bubble Sort Visualization Tool.py
+++ Topic 2/Bubble Sort Visualization Tool.py
@@ -12,3 +12,15 @@ for i in range(n - 1):
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     print("After Pass", i + 1, ":", arr)
+#------------output--------#
+Enter number of elements: 5
+Enter element 1: 5
+Enter element 2: 1
+Enter element 3: 4
+Enter element 4: 5
+Enter element 5: 6
+Original Array: [5, 1, 4, 5, 6]
+After Pass 1 : [1, 4, 5, 5, 6]
+After Pass 2 : [1, 4, 5, 5, 6]
+After Pass 3 : [1, 4, 5, 5, 6]
+After Pass 4 : [1, 4, 5, 5, 6]
