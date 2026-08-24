367a33aaa9f0592f775741abe06d8b5313e9574b
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:48:32 +0530

    Update and rename exp3-3.3.py to Real-Time Stock Price Feed.py

--- Topic 2/Real-Time Stock Price Feed.py
+++ Topic 2/Real-Time Stock Price Feed.py
@@ -18,3 +18,13 @@ for i in range(n):
 print("Sorted Prices:", prices)
 print("Minimum Price:", prices[0])
 print("Maximum Price:", prices[-1])
+#-----------OUTPUT----------#
+Enter number of stock prices: 5
+Enter price 1: 102.5
+Enter price 2: 98.3
+Enter price 3: 102.8
+Enter price 4: 78.9
+Enter price 5: 97.5
+Sorted Prices: [78.9, 97.5, 98.3, 102.5, 102.8]
+Minimum Price: 78.9
+Maximum Price: 102.8
