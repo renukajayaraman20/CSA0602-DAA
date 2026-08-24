db22626434496ae17380e20a25f46eb2063aeda2
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-24 11:10:44 +0530

    Add Heap Sort implementation

--- Topic 1/Heap Sort.py
+++ Topic 1/Heap Sort.py
@@ -5,3 +5,6 @@ sorted_arr = []
 while arr:
     sorted_arr.append(heapq.heappop(arr))
 print("Sorted Array:", sorted_arr)
+#-----------OUTPUT-----------#
+Enter array elements: 4 10 3 5 1
+Sorted Array: [1, 3, 4, 5, 10]
