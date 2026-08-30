b11c85ebcf82c4f66ae6967ee7494ae009d7c8d1
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-30 22:59:12 +0530

    Update Q7 – Brute Force String Matching – Case-Insensitive Search.py

--- Topic 3/Q7 – Brute Force String Matching – Case-Insensitive Search.py
+++ Topic 3/Q7 – Brute Force String Matching – Case-Insensitive Search.py
@@ -21,3 +21,7 @@ for i in range(len(text) - len(pattern) + 1):
 
 else:
     print("Pattern not found")
+#OUTPUT#
+Enter text: DataStructuresAndAlgorithms
+Enter pattern: ALGORITHMS
+Pattern found at position 17
