59863d6a99013338ff50afdb17d28214e5e2751e
Renuka Jayaraman <192573007.simats@saveetha.com>
2026-08-30 22:56:02 +0530

    Update Q3 – Brute Force String Matching – Comparison Table.py

--- Topic 3/Q3 – Brute Force String Matching – Comparison Table.py
+++ Topic 3/Q3 – Brute Force String Matching – Comparison Table.py
@@ -21,3 +21,14 @@ for i in range(len(text) - len(pattern) + 1):
     print("Shift =", i,
           "Comparisons =", comparisons,
           "Result =", result)
+    #----------OUTPUT-------#
+Enter text: MISSISSIPPI
+Enter pattern: ISSI
+Shift = 0 Comparisons = 1 Result = Mismatch
+Shift = 1 Comparisons = 4 Result = Match
+Shift = 2 Comparisons = 1 Result = Mismatch
+Shift = 3 Comparisons = 1 Result = Mismatch
+Shift = 4 Comparisons = 4 Result = Match
+Shift = 5 Comparisons = 1 Result = Mismatch
+Shift = 6 Comparisons = 1 Result = Mismatch
+Shift = 7 Comparisons = 2 Result = Mismatch
